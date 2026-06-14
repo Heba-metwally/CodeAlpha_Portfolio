5bb81d1c33304c2d7600b2bddad41a18886fdc06
heba-metwally <hebametwally490@gmail.com>
2026-06-15 02:42:51 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        const offset = 70;
+        const top = target.getBoundingClientRect().top + window.scrollY - offset;
+        window.scrollTo({ top, behavior: 'smooth' });
+    });
+});
